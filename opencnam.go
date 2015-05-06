3e7d49d9f764b27e222036bab0f21ac680d291e1
Michael West <mwest@mdsol.com>
2015-05-06 10:00:39 -0400

    Initial commit of OpenCNAM CLI utility

---
+++ opencnam.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/codegangsta/cli"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "OpenCNAM Querier"
+	app.Usage = "Provide a phone number and the Caller ID will be returned"
+	app.Commands = []cli.Command{
+		{
+			Name:      "query",
+			ShortName: "q",
+			Usage:     "Query for the Caller ID",
+			Action: func(c *cli.Context) {
+				phoneNumber := c.Args().First()
+
+				println("Caller ID for ", phoneNumber)
+
+				parts := []string{"https://api.opencnam.com/v2/phone", phoneNumber}
+				resp, err := http.Get(strings.Join(parts, "/"))
+				if err != nil {
+					println("Error retrieving Caller ID")
+				} else {
+					defer resp.Body.Close()
+					body, _ := ioutil.ReadAll(resp.Body)
+
+					fmt.Printf("%s\n", string(body))
+				}
+			},
+		},
+	}
+
+	app.Run(os.Args)
+}
